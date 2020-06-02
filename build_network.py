f86214c69d63780e0ca5d10b779160d44fbdb6bf
Potoole99 <30417503+Potoole99@users.noreply.github.com>
2020-06-02 09:16:02 -0400

    Formatting build

--- build_network.py
+++ build_network.py
@@ -1,2 +1,8 @@
-class BuildNetwork(num_input, num_output, max_input, max_output, accuracy)
-    
+import base_node
+import input_node
+import output_node
+
+def main(num_input, num_output, max_input, max_output, accuracy):
+    input_node_array = []
+    for i in range(num_input):
+        input_node_array.append(input_node())
