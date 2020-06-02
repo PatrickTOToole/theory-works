1f16a30b708e4f1f0fcd7347b93da3506ec304d6
Potoole99 <30417503+Potoole99@users.noreply.github.com>
2020-06-02 18:18:35 -0400

    Editted execution pipeline

--- src/Synapse.py
+++ src/Synapse.py
@@ -9,10 +9,13 @@ class Synapse:
     weight = 0
     buffer = 0
 
+    def pull_buffer(self):
+        temp = self.buffer
+        self.buffer = 0
+        temp = temp * self.weight
+        return temp
 
-def pull_buffer(synapse):
-    temp = synapse.buffer
-    synapse.buffer = 0
-    return temp
+    def push_buffer(self, data):
+        self.buffer = data
 
 
--- src/node.py
+++ src/node.py
@@ -1,3 +1,6 @@
+from src import Synapse
+
+
 def __init__():
     print("")
 
@@ -5,19 +8,25 @@ def __init__():
 # An object to store data for a singular node
 class BaseNode:
     weight = 0
-    output_nodes = []
-    input_nodes = []
+    output_synapses = []
+    input_synapses = []
 
-    def process(self, input_val):
-        return input_val * self.weight
+    def process(self):
+        data = 0
+        for in_synapse in range(len(self.input_synapses)):
+            buffer = self.input_synapses[in_synapse].pull_buffer()
+            buffer = buffer * self.weight
+            data += buffer
+        for out_synapse in range(len(self.output_synapses)):
+            self.output_synapses[out_synapse].push_buffer()
 
     def btf_learn(self, weight_val, direction):
         self.weight = self.weight - (0.5 * weight_val * direction)
         weight_val = weight_val * 0.5
-        for i in range(len(self.input_nodes)):
-            self.input_nodes[i].btf_learn(weight_val, direction)
+        for i in range(len(self.input_synapses)):
+            self.input_synapses[i].btf_learn(weight_val, direction)
 
     def ftb_learn(self, weight_val, direction):
         self.weight = self.weight - (0.5 * weight_val * direction)
-        for i in range(len(self.output_nodes)):
-            self.output_nodes[i].btf_learn(weight_val, direction)
+        for i in range(len(self.output_synapses)):
+            self.output_synapses[i].btf_learn(weight_val, direction)
