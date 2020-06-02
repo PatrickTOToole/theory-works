b687e4696ad3a915cb9490d74ba92bc4ed75aed8
Potoole99 <30417503+Potoole99@users.noreply.github.com>
2020-06-02 10:29:15 -0400

    Update build_network.py

--- build_network.py
+++ build_network.py
@@ -2,6 +2,7 @@ import base_node
 import input_node
 import output_node
 
+
 def main(num_input, num_output, max_input, max_output, depth):
     input_node_array = []
     output_node_array = []
@@ -28,24 +29,36 @@ def propagate_layers(input_layers, output_layers, input_num, output_num, layers)
     total_layers = output_layers + input_layers
     for i in range(total_layers + 1):
         layers.append([])
-    for i in range(input_num):
-        for j in range(input_layers):
-            if j == 0:
-                continue
-            if input_layers > input_num:
-                if j < (input_layers - input_num):
-                    layers[j].append([base_node()])
-                    layers[j].append([base_node()])
-                elif j == 0:
-                    layers[j].append([base_node()])
+    propagate_layers_auxiliary(input_num, input_layers, total_layers, layers, 0)
     layers[input_layers + 1].append([base_node])
-    for i in range(output_num):
-        for j in range(output_layers):
+    propagate_layers_auxiliary(output_num, output_layers, total_layers, layers, 1)
+
+def propagate_layers_auxiliary(num, num_layers, total_layers, layers, mode):
+    for i in range(num):
+        for j in range(num_layers):
+            if mode == 0:
+                x = j
+            else:
+                x = total_layers - j
             if j == 0:
                 continue
-            if output_layers > output_num:
-                if j < (output_layers - output_num):
-                    layers[total_layers - j].append([base_node()])
-                    layers[total_layers - j].append([base_node()])
+            if num_layers > num:
+                if j < (num_layers - num):
+                    layers[x].append([base_node()])
+                    layers[x].append([base_node()])
                 elif j == 0:
-                    layers[total_layers - j].append([base_node()])
+                    layers[x].append([base_node()])
+                else:
+                    if i > j:
+                        layers[x].append([base_node()])
+            elif num_layers < num:
+                if j > (num_layers - num):
+                    layers[x].append([base_node()])
+                elif j == 0:
+                    layers[x].append([base_node()])
+                else:
+                    continue
+            else:
+                if i > j:
+                    layers[x].append([base_node()])
+
