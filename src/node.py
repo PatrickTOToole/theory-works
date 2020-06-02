367e4b1c8119223472e51a371ad86c499d3b3cb6
Potoole99 <30417503+Potoole99@users.noreply.github.com>
2020-06-02 18:21:03 -0400

    Removed learning code

--- src/node.py
+++ src/node.py
@@ -21,12 +21,8 @@ class BaseNode:
             self.output_synapses[out_synapse].push_buffer()
 
     def btf_learn(self, weight_val, direction):
-        self.weight = self.weight - (0.5 * weight_val * direction)
-        weight_val = weight_val * 0.5
-        for i in range(len(self.input_synapses)):
-            self.input_synapses[i].btf_learn(weight_val, direction)
+        print("")
 
     def ftb_learn(self, weight_val, direction):
-        self.weight = self.weight - (0.5 * weight_val * direction)
-        for i in range(len(self.output_synapses)):
-            self.output_synapses[i].btf_learn(weight_val, direction)
+        print("")
+
