03f3de17129a24bfda14e79707015be2d482d4e9
Potoole99 <30417503+Potoole99@users.noreply.github.com>
2020-06-02 09:17:32 -0400

    Formatting build

--- build_network.py
+++ build_network.py
@@ -4,5 +4,8 @@ import output_node
 
 def main(num_input, num_output, max_input, max_output, accuracy):
     input_node_array = []
+    output_node_array = []
     for i in range(num_input):
         input_node_array.append(input_node())
+    for i in range(num_output):
+        output_node_array.append(output_node())
