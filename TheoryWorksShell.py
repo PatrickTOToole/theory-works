ea7a7b6f1233c17fbcae8bfa101e396292d08deb
Potoole99 <30417503+Potoole99@users.noreply.github.com>
2020-06-19 13:34:31 -0400

    Changed Command format and execution

--- TheoryWorksShell.py
+++ TheoryWorksShell.py
@@ -6,14 +6,13 @@ import json
 from pathlib import Path
 import os
 from src.commands import save_network
-
+from src.commands import build_network
 
 argv = sys.argv
 current_network = None
-current_command = ""
+c_argv = ""
 cwd = Path(os.path.dirname(os.path.abspath(__file__)));
 config = json.load(open(cwd / "config.json"))
-print(config["commands"]["build"])
 if len(argv) > 1 and argv[1] == "-f":
     if len(argv) > 3:
         current_network = build_from_file(argv[2])
@@ -22,33 +21,45 @@ elif len(argv) > 1 and argv[1] == "-b":
         current_network = generate_network(argv[2:6])
 while True:
     try:
-        current_command = str(input("TWshell>"));
+        c_argv = str(input("TWshell>")).split(" ");
+        for idx in range(len(c_argv)):
+            if c_argv[idx] == "cn":
+                c_argv[idx] = current_network
     except KeyboardInterrupt:
-        print("To quit, enter \"quit\"")
+        print("Interrupt Detected\nTo quit, enter \"quit\"")
         continue
-    if current_command in config["commands"]:
-        temp = config["commands"][current_command]
-        temp += f"({current_network}, \"temp\")"
-        exec(temp)
-        sys.exit(0)
-    if current_command == "build":
+    if c_argv[0] in config["commands"]:
+        cmd_len = len(c_argv)
+        if config["commands"][c_argv[0]][1] == 0:
+            exec(config["commands"][c_argv[0]][0])
+        else:
+            temp = config["commands"][c_argv[0]][0]
+            temp += "("
+            for elt in range(len(c_argv[:cmd_len - 1])):
+                temp += f"{c_argv[elt]}, "
+            temp += f"{c_argv[cmd_len-1]}"
+            temp += ")"
+            print(temp + "\n")
+            exec(temp)
+    else:
+        continue
+    """if c_argv == "build":
         current_network = generate_network(3, 3, 2)
         print("Network built")
-    elif current_command == "save network":
+    elif c_argv == "save network":
         if current_network is None:
             print("No current network")
         else:
             file = generate_network_file(current_network, "temp")
             print("Network saved")
-    elif current_command == "clear network":
+    elif c_argv == "clear network":
         current_network = None
         print("Network cleared")
-    elif current_command == "build from file":
+    elif c_argv == "build from file":
         if current_network is not None:
             print("Current network exists")
         else:
             current_network = build_from_file("temp.csv")
-    elif current_command == "quit":
+    elif c_argv == "quit":
         sys.exit(0)
-    else:
-        print("Invalid Command")
+    else:"""
--- src/commands.py
+++ src/commands.py
@@ -7,21 +7,21 @@ def __init__():
 
 
 def build_network(*args):
-    if len(args) != 3:
-        print("Invalid arguments\nUsage: build: <num inputs> <num outputs> <depth>")
+    if len(args) != 4:
+        print(f"Invalid arguments {len(args)}\nUsage: build: <num inputs> <num outputs> <depth>")
         return -1
     else:
-        return generate_network(*args)
+        return generate_network(*args[1:])
 
 
 def save_network(*args):
-    if len(args) != 2:
-        print("Invalid arguments\nUsage: save <filename>")
+    if len(args) != 3:
+        print(f"Invalid arguments {len(args)}\nUsage: save <network> <filename>")
         return -1
     else:
         if args[0] is None:
             print("No current network")
             return -1
         else:
-            print(f"Network saved as {generate_network_file(*args)}")
+            print(f"Network saved as {generate_network_file(*args[1:])}")
 
